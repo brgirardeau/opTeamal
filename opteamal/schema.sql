26515c5f71e228068b72189696c417b69646cb7a
Johan Widmann <jw69@rice.edu>
2017-10-28 15:34:02 -0500

    replace schema.sql

--- opteamal/schema.sql
+++ opteamal/schema.sql
@@ -1,6 +1,17 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  text text not null
+
+drop table if exists players;
+create table players (
+	id integer primary key autoincrememnt,
+	name text not null,
+	position text not null,
+	team text not null,
+	captain integer not null
 );
+
+
+drop table if exists team;
+create table team (
+	id integer primary key autoincrememnt,
+	name text not null,
+
+)
