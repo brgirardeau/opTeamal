2126dd519ac02e29529ff3689602173ca208f13d
Johan Widmann <jw69@rice.edu>
2017-10-28 15:44:33 -0500

    schema update

--- opteamal/schema.sql
+++ opteamal/schema.sql
@@ -1,15 +1,4 @@
 
-drop table if exists players;
-create table players (
-	id integer primary key autoincrememnt,
-	name text not null,
-	position text not null,
-	team text not null,
-	captain integer not null,
-	lineupId integer not null
-);
-
-
 drop table if exists team;
 create table team (
 	id integer primary key autoincrememnt,
@@ -21,12 +10,37 @@ create table team (
 drop table if exists analyzer;
 create table analyzer (
 	id integer primary key autoincrement,
-	team text not null,
+	teamID text not null,
+		foreign key (teamId) references team(id)
 
 );
 
 drop table if exists lineup;
 create table lineup (
 	id integer primary key autoincrement,
+	teamId integer not null,
+		foreign key (teamId) references team(id)
+);
 
+drop table if exists statistics;
+create table statistics (
+	id integer primary key autoincrememnt,
+	teamId integer not null,
+		foreign key (teamId) references team(id),
+	name text not null,
+	pro integer not null,
+	weight real not null
+
+)
+
+drop table if exists players;
+create table players (
+	id integer primary key autoincrememnt,
+	name text not null,
+	position text not null,
+	team text not null,
+	captain integer not null,
+		foreign key (teamId) references team(id),
+	lineupId integer not null,
+		foreign key (lineupId) references lineup(id)
 );
